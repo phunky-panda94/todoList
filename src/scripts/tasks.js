997f3d42dd9916ed9deef54838e32e7e2e127f91
phunky-panda94 <victor.nim94@outlook.com>
2021-09-26 11:36:29 +1000

    implemented edit functionality

--- src/scripts/tasks.js
+++ src/scripts/tasks.js
@@ -3,27 +3,48 @@ let tasks = [];
 
 class Task {
 
-    constructor(project, name, dueDate, notes) {
+    constructor(id, project, name, dueDate, notes) {
+        this._id = id;
         this._project = project;
         this._name = name;
         this._dueDate = dueDate;
         this._notes = notes;
     }
 
-    get project () {
+    get id() {
+        return this._id;
+    }
+    
+    get project() {
         return this._project;
     }
 
-    get name () {
+    set project(project) {
+        this._project = project;
+    }
+
+    get name() {
         return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
     }
 
     get dueDate() {
         return this._dueDate;
     }
 
+    set dueDate(date) {
+        this._dueDate = date;
+    }
+
     get notes() {
         return this._notes;
+    }
+
+    set notes(notes) {
+        this._notes = notes;
     }
 
 }
@@ -47,8 +68,13 @@ const notes = document.querySelector('#notes-text');
 const cross = document.querySelector('#close');
 const container = document.querySelector('#tasks-container');
 
+// event listeners
+
 btn.addEventListener('click', () => {
 
+    let taskId = tasks.length
+    form.taskId = taskId;
+    console.log(taskId);
     modal.classList.toggle('hidden');
 
 });
@@ -56,6 +82,11 @@ btn.addEventListener('click', () => {
 cross.addEventListener('click', () => {
 
     modal.classList.toggle('hidden');
+    
+    // change buttons
+    deleteBtn.style.display = 'none';
+    add.textContent = 'Add';
+
     form.reset();
 
 });
@@ -63,6 +94,11 @@ cross.addEventListener('click', () => {
 cancel.addEventListener('click', () => {
 
     modal.classList.toggle('hidden');
+
+    // change buttons
+    deleteBtn.style.display = 'none';
+    add.textContent = 'Add';
+
     form.reset();
 
 })
@@ -79,27 +115,46 @@ form.addEventListener('submit', (e) => {
 
     e.preventDefault();
 
-    // create task object
-    let task = new FormData(form);
-    let newTask = new Task(task.get('project'), task.get('name'), task.get('date'), task.get('notes'));
+    // if task exists, update task
+    if (form.taskId in tasks) {
 
-    // add task
-    addTask(newTask);
-    console.log('Adding ' + task.get('name'))
-    
-    // close and reset form
-    modal.classList.toggle('hidden');
-    form.reset();
+        let task = new FormData(form);
+        let existingTask = tasks[form.taskId]
 
-    // update display
-    updateTasks();
-    
+        existingTask.project = task.get('project');
+        existingTask.name = task.get('name');
+        existingTask.dueDate = task.get('date');
+        existingTask.notes = task.get('notes');
+
+        updateTask(form.taskId);
+
+    // else add new task
+    } else {
+
+        // create task object
+        let task = new FormData(form);
+        let newTask = new Task(form.taskId, task.get('project'), task.get('name'), task.get('date'), task.get('notes'));
+
+        // add task
+        addTask(newTask);
+        console.log(`Adding: taskId-${form.taskId} name-${task.get('name')}`);
+        
+        // close and reset form
+        modal.classList.toggle('hidden');
+        form.reset();
+
+        // update display
+        displayNewTask(form.taskId);
+
+    }
+
 })
 
-function updateTasks() {
+// functions
 
-    let taskIndex = tasks.length -1
-    let task = tasks[taskIndex];
+function displayNewTask(taskId) {
+
+    let task = tasks[taskId];
 
     taskCard = document.createElement('div');
     taskCard.classList.add('card');
@@ -130,24 +185,24 @@ function updateTasks() {
     taskItem.append(taskName);
 
     let clickArea = document.createElement('div');
-    clickArea.classList.add('click-area', 'border');
-    clickArea.taskId = taskIndex;
+    clickArea.classList.add('click-area');
+    clickArea.taskId = taskId;
     clickArea.addEventListener('click', editTask);
 
     taskCard.append(heading);
     taskCard.append(taskItem);
     taskCard.append(clickArea);
-    taskCard.setAttribute('id', `task-${taskIndex}`)
+    taskCard.setAttribute('id', `task-${taskId}`)
     container.appendChild(taskCard);
 
 }
 
 function editTask(e) {
 
-    let taskIndex = e.currentTarget.taskId;
+    let taskId = e.currentTarget.taskId;
 
     // populate with task details
-    let task = tasks[taskIndex];
+    let task = tasks[taskId];
     project.value = task.project;
     name.value = task.name;
     date.value = task.dueDate;
@@ -156,29 +211,46 @@ function editTask(e) {
     // change buttons
     add.textContent = 'Save changes'
     cancel.textContent = 'Cancel'
-
-    deleteBtn.taskId = taskIndex;
     deleteBtn.style.display = 'block';
+
+    form.taskId = taskId;
     modal.classList.toggle('hidden');
 
 }
 
-function deleteTask(e) {
+function deleteTask() {
     
-    let taskIndex = e.currentTarget.taskId;
-
     // close and clear form
     modal.classList.toggle('hidden');
     form.reset();
 
     // remove task from tasks
-    console.log(`taskId: ${taskIndex}`)
-    tasks.splice(taskIndex,1);
-    console.log(tasks);
+    console.log(`taskId: ${form.taskId}`)
+    tasks.splice(form.taskId,1);
+    console.log(`Tasks: ${tasks}`);
 
     // remove card
-    let taskToRemove = document.querySelector(`#task-${taskIndex}`);
+    let taskToRemove = document.querySelector(`#task-${form.taskId}`);
 
     taskToRemove.remove();
+
+}
+
+function updateTask(taskId) {
+
+    // get task
+    let updatedTask = tasks[taskId];
+    console.log(updatedTask);
+
+    // update details
+    let project = document.querySelectorAll(`#task-${taskId} span`)[0];
+    console.log(project);
+    project.textContent = updatedTask.project;
+
+    let taskName = document.querySelectorAll(`#task-${taskId} span`)[1];
+    taskName.textContent = updatedTask.name;
+
+    modal.classList.toggle('hidden');
+    form.reset();
 
 }
