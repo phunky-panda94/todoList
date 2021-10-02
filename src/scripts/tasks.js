02a1fe489286d87d697ccc46996ed2dee656f513
phunky-panda94 <victor.nim94@outlook.com>
2021-10-02 11:01:58 +1000

    added day countdown

--- src/scripts/tasks.js
+++ src/scripts/tasks.js
@@ -1,47 +1,63 @@
 class Task {
 
-    constructor(id, project, name, dueDate, notes) {
-        this._id = id;
-        this._project = project;
-        this._name = name;
-        this._dueDate = dueDate;
-        this._notes = notes;
+    #id;
+    #project;
+    #name;
+    #date;
+    #notes;
+
+    constructor(id, project, name, date, notes) {
+        this.#id = id;
+        this.#project = project;
+        this.#name = name;
+        this.#date = date;
+        this.#notes = notes;
     }
 
     get id() {
-        return this._id;
+        return this.#id;
     }
     
     get project() {
-        return this._project;
+        return this.#project;
     }
 
     set project(project) {
-        this._project = project;
+        this.#project = project;
     }
 
     get name() {
-        return this._name;
+        return this.#name;
     }
 
     set name(name) {
-        this._name = name;
+        this.#name = name;
     }
 
-    get dueDate() {
-        return this._dueDate;
+    get date() {
+        return this.#date;
     }
 
-    set dueDate(date) {
-        this._dueDate = date;
+    set date(date) {
+        this.#date = date;
     }
 
     get notes() {
-        return this._notes;
+        return this.#notes;
     }
 
     set notes(notes) {
-        this._notes = notes;
+        this.#notes = notes;
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            project: this.project,
+            name: this.name,
+            date: this.date,
+            notes: this.notes
+        }
     }
 
 }
@@ -61,8 +77,7 @@ const notes = document.querySelector('#notes-text');
 const cross = document.querySelector('#close');
 const container = document.querySelector('#tasks-container');
 
-// event listeners
-
+/* event listeners */
 btn.addEventListener('click', () => {
 
     let taskId = crypto.randomUUID();
@@ -116,7 +131,7 @@ form.addEventListener('submit', (e) => {
 
         existingTask.project = task.get('project');
         existingTask.name = task.get('name');
-        existingTask.dueDate = task.get('date');
+        existingTask.date = task.get('date');
         existingTask.notes = task.get('notes');
 
         updateTask(form.taskId);
@@ -175,8 +190,13 @@ function displayNewTask(taskId) {
     heading.classList.add('card-heading', 'flex', 'flex-ai-c', 'flex-jc-sb');
 
     let project = document.createElement('span');
-    project.textContent = task._project;
+    project.textContent = task.project;
     heading.append(project);
+
+    let date = document.createElement('span');
+    let daysLeft = calculateDaysLeft(task.date);
+    date.textContent = daysLeft;
+    heading.append(date);
 
     let star = document.createElement('input');
     star.type = 'checkbox';
@@ -192,7 +212,7 @@ function displayNewTask(taskId) {
     taskItem.append(checkbox);
 
     let taskName = document.createElement('span');
-    taskName.textContent = task._name;
+    taskName.textContent = task.name;
     taskItem.append(taskName);
 
     let clickArea = document.createElement('div');
@@ -203,7 +223,7 @@ function displayNewTask(taskId) {
     taskCard.append(heading);
     taskCard.append(taskItem);
     taskCard.append(clickArea);
-    taskCard.setAttribute('id', `task-${taskId}`)
+    taskCard.setAttribute('id', `${taskId}`)
     container.appendChild(taskCard);
 
 }
@@ -214,10 +234,10 @@ function editTask(e) {
 
     // populate with task details
     let task = JSON.parse(localStorage.getItem(taskId));
-    project.value = task._project;
-    name.value = task._name;
-    date.value = task._dueDate;
-    notes.value = task._notes;
+    project.value = task.project;
+    name.value = task.name;
+    date.value = task.date;
+    notes.value = task.notes;
 
     // change buttons
     add.textContent = 'Save changes'
@@ -239,7 +259,7 @@ function deleteTask() {
     localStorage.removeItem(form.taskId);
 
     // remove card
-    let taskToRemove = document.querySelector(`#task-${form.taskId}`);
+    let taskToRemove = document.querySelector(`#${form.taskId}`);
 
     taskToRemove.remove();
 
@@ -252,15 +272,32 @@ function updateTask(taskId) {
     console.log(updatedTask);
 
     // update details
-    let project = document.querySelectorAll(`#task-${taskId} span`)[0];
+    let project = document.querySelectorAll(`#${taskId} span`)[0];
     console.log(project);
     project.textContent = updatedTask.project;
 
-    let taskName = document.querySelectorAll(`#task-${taskId} span`)[1];
+    let taskName = document.querySelectorAll(`#${taskId} span`)[1];
     taskName.textContent = updatedTask.name;
 
     modal.classList.toggle('hidden');
     form.reset();
+
+}
+
+function calculateDaysLeft(taskDate) {
+
+    let dateComponents = taskDate.split('-');
+    let dueDate = new Date(dateComponents[0], dateComponents[1] - 1, dateComponents[2]);
+    let today = new Date();
+
+    // calculate days left / overdue
+    let daysLeft = Math.floor((dueDate - today) / (1000 * 60 * 60 * 24));
+
+    if (daysLeft < 0) {
+        return `${daysLeft} days overdue`;
+    } else {
+        return `${Math.abs(daysLeft)} days left`;
+    }
 
 }
 
