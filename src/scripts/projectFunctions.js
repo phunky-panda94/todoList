2a908b3a7524a5e5c5a2589e4560f1005fd412e8
phunky-panda94 <victor.nim94@outlook.com>
2021-10-10 15:07:52 +1100

    implemented add project functionality

---
+++ src/scripts/projectFunctions.js
@@ -0,0 +1,73 @@
+/* DOM */
+const myProjects = document.querySelector('#projects');
+
+// TODO: display projects as cards
+export function displayProjects() {
+
+    // get projects
+    if (localStorage.getItem('projects') != null) {
+
+        let projects = JSON.parse(localStorage.getItem('projects'));
+
+        for (let project of projects) {
+            displayProject(project);
+        }
+
+    }
+
+}
+
+// TODO: add project to local storage
+export function addProject(project) {
+
+    let projects;
+
+    // if no projects, create new array
+    if (localStorage.getItem('projects') == null) {
+        projects = [project];
+    } else {
+        // Add project to project array
+        projects = JSON.parse(localStorage.getItem('projects'));
+        projects.push(project);
+    }
+
+    // Store updated project array in local storage
+    localStorage.setItem('projects', JSON.stringify(projects));
+
+}
+
+export function removeProject(project) {
+
+    // Remove project from project array
+    let projects = JSON.parse(localStorage.getItem('projects'));
+    let index = projects.indexOf(project);
+    projects.splice(index,1);
+
+    // Store updated project array in local storage
+    localStorage.setItem('project', JSON.stringify(projects));
+
+    if (myProjects != null) {
+        let projectCard = document.querySelector(`#${project}`);
+        projectCard.remove();
+    }
+
+}
+
+export function displayProject(project) {
+
+    // create card
+    let card = document.createElement('div');
+    card.style.cursor = 'pointer';
+    card.classList.add('card', 'flex', 'flex-jc-c', 'flex-ai-c');
+
+    let name = document.createElement('h2');
+    name.textContent = project;
+
+    card.append(name);
+
+    // add card
+    myProjects.append(card);
+
+}
+
+displayProjects();
--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -1,8 +1,41 @@
+import { addProject, displayProject } from "./projectFunctions.js";
+
 const menu = document.querySelector('#menu');
 const sidebar = document.querySelector('#sidebar');
+const openForm = document.querySelector('#open');
+const modal = document.querySelector('.modal');
+const closeForm = document.querySelector('#close');
+const form = document.querySelector('form');
 
 menu.addEventListener('click', () => {
 
     sidebar.classList.toggle("hidden");
+
+});
+
+openForm.addEventListener('click', () => {
+
+    modal.classList.toggle('hidden');
+
+});
+
+closeForm.addEventListener('click', () => {
+
+    modal.classList.toggle('hidden');
+
+});
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    let project = e.target.elements['project'].value
+
+    addProject(project);
+
+    modal.classList.toggle('hidden');
+    form.reset();
+
+    displayProject(project);
 
 });
